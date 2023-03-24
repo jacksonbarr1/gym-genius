e23681db81c011106e2ac01973fdb1aa165f8295
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-23 21:20:59 -0400

    Implement HTTP Basic Auth

--- src/main/java/com/example/gymgenius/controller/HomeController.java
+++ src/main/java/com/example/gymgenius/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1/home")
 public class HomeController {
 
     @ResponseBody
