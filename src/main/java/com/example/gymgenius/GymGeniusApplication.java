873d9f853ed49b0f2872f1ba0b677a2915d763f6
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-24 00:57:59 -0400

    Define UserDetailsService bean

--- src/main/java/com/example/gymgenius/GymGeniusApplication.java
+++ src/main/java/com/example/gymgenius/GymGeniusApplication.java
@@ -1,13 +1,22 @@
 package com.example.gymgenius;
 
+import com.example.gymgenius.repository.UserRepository;
+import com.example.gymgenius.security.UserDetailsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SpringBootApplication
 public class GymGeniusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GymGeniusApplication.class, args);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new UserDetailsServiceImpl(userRepository);
     }
 
 }
