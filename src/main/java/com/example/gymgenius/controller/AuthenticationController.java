2593ab8d37e1d53191b879b000e8986ea3081853
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-24 16:39:48 -0400

    Change controller endpoint

--- src/main/java/com/example/gymgenius/controller/AuthenticationController.java
+++ src/main/java/com/example/gymgenius/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.example.gymgenius.controller;
 
 import com.example.gymgenius.dto.AuthenticationRequest;
+import com.example.gymgenius.dto.UserDTO;
 import com.example.gymgenius.entity.GymGeniusUser;
 import com.example.gymgenius.security.JWTUtils;
 import com.example.gymgenius.service.UserService;
@@ -8,18 +9,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
-
+    private final UserService userService;
     private final JWTUtils jwtUtils;
 
     @ResponseBody
@@ -33,6 +33,12 @@ public class AuthenticationController {
             return ResponseEntity.ok(jwtUtils.generateToken(user));
         }
         return ResponseEntity.status(400).body("Authentication error");
+    }
+
+    @ResponseBody
+    @PostMapping("/register")
+    public ResponseEntity<GymGeniusUser> register(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.register(userDTO));
     }
 
     @ResponseBody
