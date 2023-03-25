e9c9999bbe4624093e1d85ab144a39b0f66d17ea
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-24 20:39:04 -0400

    Update register to return JWT token

--- src/main/java/com/example/gymgenius/controller/UserController.java
+++ src/main/java/com/example/gymgenius/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.gymgenius.controller;
 
 import com.example.gymgenius.dto.AuthenticationRequest;
+import com.example.gymgenius.dto.AuthenticationResponse;
 import com.example.gymgenius.dto.UserDTO;
 import com.example.gymgenius.entity.GymGeniusUser;
 import com.example.gymgenius.security.JWTUtils;
@@ -24,21 +25,23 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
 
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),
                 request.getPassword()));
         final GymGeniusUser user = (GymGeniusUser) userDetailsService.loadUserByUsername(request.getEmail());
         if (user != null) {
-            return ResponseEntity.ok(jwtUtils.generateToken(user));
+            return ResponseEntity.ok(new AuthenticationResponse(jwtUtils.generateToken(user)));
         }
-        return ResponseEntity.status(400).body("Authentication error");
+        return ResponseEntity.status(400).body(new AuthenticationResponse());
     }
 
     @ResponseBody
     @PostMapping("/register")
-    public ResponseEntity<GymGeniusUser> register(@RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(userService.register(userDTO));
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        UserDTO userDTO = new UserDTO(request.getEmail(), request.getPassword());
+        GymGeniusUser user = userService.register(userDTO);
+        return authenticate(request);
     }
 
     @ResponseBody
