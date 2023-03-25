166ca4985876293208def492167549a1af592502
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-24 20:37:56 -0400

    Add Lombok annotations for DTOs

--- src/main/java/com/example/gymgenius/dto/AuthenticationRequest.java
+++ src/main/java/com/example/gymgenius/dto/AuthenticationRequest.java
@@ -1,11 +1,10 @@
 package com.example.gymgenius.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
 
