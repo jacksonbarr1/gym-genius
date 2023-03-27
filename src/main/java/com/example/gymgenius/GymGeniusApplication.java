fe65601ebd3f9de889ecfe98503c1d7e2aa533ea
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-26 22:59:51 -0400

    Enable AspectJ AutoProxy

--- src/main/java/com/example/gymgenius/GymGeniusApplication.java
+++ src/main/java/com/example/gymgenius/GymGeniusApplication.java
@@ -5,9 +5,11 @@ import com.example.gymgenius.security.UserDetailsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class GymGeniusApplication {
 
     public static void main(String[] args) {
