10326092e0245f08d7d5476f5f5aa2d3590a3689
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-27 00:11:12 -0400

    Add React frontend registration page

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Register from "./Register";
+import './App.css';
+
+function App() {
+  return (
+    <Register
+      ></Register>
+  );
+}
+
+export default App;
