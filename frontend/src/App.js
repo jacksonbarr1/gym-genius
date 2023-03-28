622fdd0746a0a80ae49f7e28d8b3a2a80ae7d66e
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-28 02:13:39 -0400

    Add profile route to test Login.js redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Register />} />
       </Routes>
     </Router>
   );
