523c9b641474c7088bac77e43bd0632b666df33d
Jackson Barr <jacksonbarr2021@gmail.com>
2023-03-28 02:14:03 -0400

    remove error catch

--- frontend/src/services/authenticationService.js
+++ frontend/src/services/authenticationService.js
@@ -3,11 +3,7 @@ import axios from "axios"
 const baseUrl = 'http://localhost:8081/user'
 
 const logIn = async (userData) => {
-    const response = await axios.post(baseUrl + '/authenticate', userData)
-        .catch(error => console.error(error))
-    return response.data
+    return await axios.post(baseUrl + '/authenticate', userData)
 }
 
-export default {
-    logIn
-}
+export default logIn
